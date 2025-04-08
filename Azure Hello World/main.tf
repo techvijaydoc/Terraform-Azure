474ce1d9160d818c5c6add6a61f762f19557bacd
techvijaydoc <vijitech@gmail.com>
2025-04-08 16:23:19 +0530

    azure vm

--- Azure Hello World/main.tf
+++ Azure Hello World/main.tf
@@ -28,15 +28,15 @@ resource "azurerm_storage_container" "tfstoragecontainer" {
 # Creating azure virtual network
 resource "azurerm_virtual_network" "tfhellonet" {
   name = "tf-vnet-1"
-  location = azurerm_resource_group.tfhelloworld.location
-  resource_group_name = azurerm_resource_group.tfhelloworld.name
+  location = azurerm_resource_group.tflearning.location
+  resource_group_name = azurerm_resource_group.tflearning.name
   address_space = ["10.0.0.0/16"]
 }
 
 # Creating azure subnet
 resource "azurerm_subnet" "tfsubnet1" {
   name = "tf-subnet-1"
-  resource_group_name = azurerm_resource_group.tfhelloworld.name
+  resource_group_name = azurerm_resource_group.tflearning.name
   virtual_network_name = azurerm_virtual_network.tfhellonet.name
   address_prefixes = ["10.0.1.0/24"]
 }
@@ -44,8 +44,8 @@ resource "azurerm_subnet" "tfsubnet1" {
 # Creating virtual interface card
 resource "azurerm_network_interface" "tfnic1" {
   name = "tf-nic-1"
-  location = azurerm_resource_group.tfhelloworld.location
-  resource_group_name = azurerm_resource_group.tfhelloworld.name
+  location = azurerm_resource_group.tflearning.location
+  resource_group_name = azurerm_resource_group.tflearning.name
 
   ip_configuration {
     name = "tf-ipconfig-1"
@@ -57,8 +57,8 @@ resource "azurerm_network_interface" "tfnic1" {
 # Creating azure virtual machine
 resource "azurerm_linux_virtual_machine" "tflinuxvm1" {
   name = "tf-linux-vm-1"
-  location = azurerm_resource_group.tfhelloworld.location
-  resource_group_name = azurerm_resource_group.tfhelloworld.name
+  location = azurerm_resource_group.tflearning.location
+  resource_group_name = azurerm_resource_group.tflearning.name
   size = "Standard_DS1_v2"
   admin_username = "adminuser"
   admin_password = "TF@azure1234"
